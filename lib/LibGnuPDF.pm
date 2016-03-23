31320d51f0c0b68e387603785974bf65dd49117b
David Warring <david.warring@gmail.com>
2016-03-24 08:17:47 +1300

    test simple encode/decode round-trip

--- lib/LibGnuPDF.pm
+++ lib/LibGnuPDF.pm
@@ -11,7 +11,8 @@ module LibGnuPDF {
     #| see pdf.h
     my constant pdf_bool_t is export(:types, :DEFAULT) = NativeCall::Types::bool;
     my constant pdf_domain_t is export(:types, :DEFAULT) = NativeCall::Types::long;
-    my constant pdf_char_t is export(:types, :DEFAULT) = uint8;
+    my constant pdf_char_t is export(:types, :DEFAULT) = int8;
+    my constant pdf_uchar_t is export(:types, :DEFAULT) = uint8;
     my constant pdf_error_domain_t is export(:types, :DEFAULT) = NativeCall::Types::long;
     my constant pdf_hash_t is export(:types, :DEFAULT) = OpaquePointer;
 
@@ -21,63 +22,112 @@ module LibGnuPDF {
     my constant pdf_status_t is export(:types, :DEFAULT) = NativeCall::Types::long;
     my constant pdf_stm_t is export(:types, :DEFAULT) = OpaquePointer;
 
-    our sub pdf_hash_new(Pointer[pdf_error_t])
+    our sub pdf_hash_new(pdf_error_t)
         returns pdf_hash_t
+        is export(:subs, :DEFAULT)
         is native(&find-lib) { * }
 
     our sub pdf_error_get_status(pdf_error_t is rw)
         returns pdf_status_t
+        is export(:subs, :DEFAULT)
         is native(&find-lib) { * }
 
     our sub pdf_error_get_domain(pdf_error_t is rw)
         returns pdf_domain_t
+        is export(:subs, :DEFAULT)
         is native(&find-lib) { * }
 
     our sub pdf_error_get_message(pdf_error_t is rw)
         returns pdf_message_t
+        is export(:subs, :DEFAULT)
         is native(&find-lib) { * }
 
-  my Int enum pdf_stm_filter_type_E is export(:types, :DEFAULT) «
-  :PDF_STM_FILTER_UNKNOWN(-1)
-  :PDF_STM_FILTER_NULL(0)
-  PDF_STM_FILTER_AHEX_ENC
-  PDF_STM_FILTER_AHEX_DEC
-  PDF_STM_FILTER_A85_ENC
-  PDF_STM_FILTER_A85_DEC
-  PDF_STM_FILTER_LZW_ENC
-  PDF_STM_FILTER_LZW_DEC
-  PDF_STM_FILTER_FLATE_ENC
-  PDF_STM_FILTER_FLATE_DEC
-  PDF_STM_FILTER_RL_ENC
-  PDF_STM_FILTER_RL_DEC
-  PDF_STM_FILTER_CCITTFAX_ENC
-  PDF_STM_FILTER_CCITTFAX_DEC
-  PDF_STM_FILTER_JBIG2_ENC
-  PDF_STM_FILTER_JBIG2_DEC
-  PDF_STM_FILTER_DCT_ENC
-  PDF_STM_FILTER_DCT_DEC
-  PDF_STM_FILTER_JPX_ENC
-  PDF_STM_FILTER_JPX_DEC
-  PDF_STM_FILTER_PRED_ENC
-  PDF_STM_FILTER_PRED_DEC
-  PDF_STM_FILTER_AESV2_ENC
-  PDF_STM_FILTER_AESV2_DEC
-  PDF_STM_FILTER_V2_ENC
-  PDF_STM_FILTER_V2_DEC
-  PDF_STM_FILTER_MD5_ENC
-  PDF_STM_FILTER_LAST
-  »;
-  my subset pdf_stm_filter_type_e of ulong where pdf_stm_filter_type_E;
-  
-  our sub pdf_stm_install_filter(pdf_stm_t,
-                                 pdf_stm_filter_type_e,
-                                 pdf_hash_t,
-                                 pdf_error_t)
-    returns pdf_bool_t
-    is native(&find-lib) { * }
+    # -- Streams --
 
-  our sub pdf_stm_supported_filter_p(pdf_stm_filter_type_e)
-    returns pdf_bool_t
-    is native(&find-lib) { * }
+    my Int enum pdf_stm_mode_E is export(:types, :DEFAULT) «
+      :PDF_STM_UNKNOWN(-1)
+      :PDF_STM_READ(0)
+       PDF_STM_WRITE
+    »;
+    our constant PDF_STM_DEFAULT_CACHE_SIZE is export(:types, :DEFAULT) = 4096;
+    my subset pdf_stm_mode_e of ulong where pdf_stm_mode_E;
+
+    my Int enum pdf_stm_filter_type_E is export(:types, :DEFAULT) «
+      :PDF_STM_FILTER_UNKNOWN(-1)
+      :PDF_STM_FILTER_NULL(0)
+       PDF_STM_FILTER_AHEX_ENC
+       PDF_STM_FILTER_AHEX_DEC
+       PDF_STM_FILTER_A85_ENC
+       PDF_STM_FILTER_A85_DEC
+       PDF_STM_FILTER_LZW_ENC
+       PDF_STM_FILTER_LZW_DEC
+       PDF_STM_FILTER_FLATE_ENC
+       PDF_STM_FILTER_FLATE_DEC
+       PDF_STM_FILTER_RL_ENC
+       PDF_STM_FILTER_RL_DEC
+       PDF_STM_FILTER_CCITTFAX_ENC
+       PDF_STM_FILTER_CCITTFAX_DEC
+       PDF_STM_FILTER_JBIG2_ENC
+       PDF_STM_FILTER_JBIG2_DEC
+       PDF_STM_FILTER_DCT_ENC
+       PDF_STM_FILTER_DCT_DEC
+       PDF_STM_FILTER_JPX_ENC
+       PDF_STM_FILTER_JPX_DEC
+       PDF_STM_FILTER_PRED_ENC
+       PDF_STM_FILTER_PRED_DEC
+       PDF_STM_FILTER_AESV2_ENC
+       PDF_STM_FILTER_AESV2_DEC
+       PDF_STM_FILTER_V2_ENC
+       PDF_STM_FILTER_V2_DEC
+       PDF_STM_FILTER_MD5_ENC
+       PDF_STM_FILTER_LAST
+    »;
+    my subset pdf_stm_filter_type_e of ulong where pdf_stm_filter_type_E;
   
+    our sub pdf_stm_supported_filter_p(pdf_stm_filter_type_e)
+        returns pdf_bool_t
+        is export(:subs, :DEFAULT)
+        is native(&find-lib) { * }
+  
+    our sub pdf_stm_mem_new(CArray[pdf_uchar_t] $buffer,
+			    pdf_size_t $size,
+			    pdf_size_t $cache-size,
+			    pdf_stm_mode_e,
+			    pdf_error_t
+		           )
+        returns pdf_stm_t
+        is export(:subs, :DEFAULT)
+        is native(&find-lib) { * }
+
+    our sub pdf_stm_get_mode(pdf_stm_t)
+        returns ulong
+        is export(:subs, :DEFAULT)
+        is native(&find-lib) { * }
+
+    our sub pdf_stm_install_filter(pdf_stm_t,
+                                   pdf_stm_filter_type_e,
+                                   pdf_hash_t,
+                                   pdf_error_t)
+        returns pdf_bool_t
+        is export(:subs, :DEFAULT)
+        is native(&find-lib) { * }
+
+    our sub pdf_stm_read(pdf_stm_t,
+                          CArray[pdf_uchar_t] $buf,
+                          pdf_size_t          $bytes,
+                          CArray[pdf_size_t]  $read-bytes,
+                          CArray[Pointer])
+        returns pdf_bool_t
+        is export(:subs, :DEFAULT)
+        is native(&find-lib) { * }
+
+    our sub pdf_stm_write(pdf_stm_t,
+                          CArray[pdf_uchar_t] $buf,
+                          pdf_size_t          $bytes,
+                          CArray[pdf_size_t]  $written-bytes,
+                          CArray[Pointer])
+        returns pdf_bool_t
+        is export(:subs, :DEFAULT)
+        is native(&find-lib) { * }
+
 }
--- t/00basic.t
+++ t/00basic.t
@@ -1,25 +1,48 @@
 use v6;
 use Test;
-use LibGnuPDF;
+use LibGnuPDF :types, :subs;
 use NativeCall;
 
 my pdf_hash_t $hash;
-my pdf_error_t $pdf_stat;
+my pdf_error_t $pdf-stat;
 
-lives-ok {$hash = LibGnuPDF::pdf_hash_new($pdf_stat)}, 'pdf_hash_new - lives';
+lives-ok {$hash = pdf_hash_new($pdf-stat)}, 'pdf_hash_new - lives';
 isa-ok $hash, pdf_hash_t;
 
-my pdf_status_t $status = LibGnuPDF::pdf_error_get_status($pdf_stat);
-is-deeply $status, 0, "normal status";
+is pdf_stm_supported_filter_p(PDF_STM_FILTER_AHEX_ENC), 1, 'AHEX_ENC is supported';
+is pdf_stm_supported_filter_p(PDF_STM_FILTER_AHEX_DEC), 1, 'AHEX_DEC is supported';
+is pdf_stm_supported_filter_p(PDF_STM_FILTER_PRED_ENC), 1, 'PRED_ENC is supported';
+is pdf_stm_supported_filter_p(PDF_STM_FILTER_DCT_ENC), 0, 'DCT_ENC is not supported';
+is pdf_stm_supported_filter_p(PDF_STM_FILTER_DCT_DEC), 1, 'DCT_DEC is supported';
 
-my pdf_domain_t $domain = LibGnuPDF::pdf_error_get_domain($pdf_stat);
-is-deeply $domain, 0, "normal domain";
+my CArray[pdf_uchar_t] $buffer .= new("hello world!".encode("latin-1"));
+my $enc-stm = pdf_stm_mem_new($buffer, $buffer.elems, 0, PDF_STM_READ, $pdf-stat);
 
-is LibGnuPDF::pdf_stm_supported_filter_p(PDF_STM_FILTER_AHEX_ENC), 1, 'AHEX_ENC is supported';
-is LibGnuPDF::pdf_stm_supported_filter_p(PDF_STM_FILTER_AHEX_DEC), 1, 'AHEX_DEC is supported';
-is LibGnuPDF::pdf_stm_supported_filter_p(PDF_STM_FILTER_PRED_ENC), 1, 'PRED_ENC is supported';
-is LibGnuPDF::pdf_stm_supported_filter_p(PDF_STM_FILTER_DCT_ENC), 0, 'DCT_ENC is not supported';
-is LibGnuPDF::pdf_stm_supported_filter_p(PDF_STM_FILTER_DCT_DEC), 1, 'DCT_DEC is supported';
+is +pdf_stm_get_mode($enc-stm), +PDF_STM_READ, 'stream mode';
+
+ok pdf_stm_install_filter($enc-stm, PDF_STM_FILTER_AHEX_ENC, $hash, $pdf-stat), 'install enc filter';
+
+my CArray[pdf_uchar_t] $buf-enc .= new;
+$buf-enc[100] = 0;
+
+my CArray[pdf_size_t] $bytes .= new;
+$bytes[0] = 0;
+pdf_stm_read($enc-stm, $buf-enc, $buf-enc.elems-1, $bytes, $pdf-stat);
+is $bytes[0], 25, "encoded length";
+my Str $encoded = buf8.new( $buf-enc[0 ..^ $bytes[0]] ).decode("latin-1");
+is $encoded, "68656C6C6F20776F726C6421>", "encoded";
+
+my $dec-stm = pdf_stm_mem_new($buf-enc, $bytes[0],
+			  PDF_STM_DEFAULT_CACHE_SIZE, PDF_STM_READ,
+			  $pdf-stat);
+
+ok pdf_stm_install_filter($dec-stm, PDF_STM_FILTER_AHEX_DEC, $hash, $pdf-stat), 'install dec filter';
+
+my CArray[pdf_uchar_t] $buf-dec .= new;
+$buf-dec[100] = 0;
+pdf_stm_read($dec-stm, $buf-dec, $buf-dec.elems-1, $bytes, $pdf-stat);
+my Str $decoded = buf8.new( $buf-dec[0 ..^ $bytes[0]] ).decode("latin-1");
+is $decoded, "hello world!", 'decoded';
 
 done-testing;
 
