d738d93c888bbf3760c7b85d91e2a6432b4e8852
David Warring <david.warring@gmail.com>
2016-03-25 10:44:50 +1300

    add PdfAlloc role - asssociates 'new' with 'destroy' methods

--- lib/LibGnuPDF.pm
+++ lib/LibGnuPDF.pm
@@ -7,20 +7,32 @@ module LibGnuPDF {
     sub find-lib is export(:find-lib) {
 	$*VM.platform-library-name('gnupdf'.IO).Str;
     }
-    
+
+    my role PdfAlloced[&destroy-sub] {
+	submethod destroy {
+	    &destroy-sub(self);
+	}
+    }
+
     #| see pdf.h
     my constant pdf_bool_t is export(:types, :DEFAULT) = NativeCall::Types::bool;
     my constant pdf_domain_t is export(:types, :DEFAULT) = NativeCall::Types::long;
     my constant pdf_char_t is export(:types, :DEFAULT) = int8;
     my constant pdf_uchar_t is export(:types, :DEFAULT) = uint8;
     my constant pdf_error_domain_t is export(:types, :DEFAULT) = NativeCall::Types::long;
-    my constant pdf_hash_t is export(:types, :DEFAULT) = OpaquePointer;
+
+    our sub pdf_hash_destroy(OpaquePointer) is export(:subs, :DEFAULT) is native(&find-lib) { * }
+    my constant pdf_hash_t is export(:types, :DEFAULT) = OpaquePointer but PdfAlloced[&pdf_hash_destroy];
 
     my constant pdf_error_t is export(:types, :DEFAULT) = OpaquePointer;
     my constant pdf_message_t is export(:types, :DEFAULT) = Pointer[CArray[uint8]];
     my constant pdf_size_t is export(:types, :DEFAULT) = NativeCall::Types::long;
-    my constant pdf_status_t is export(:types, :DEFAULT) = OpaquePointer;
-    my constant pdf_stm_t is export(:types, :DEFAULT) = OpaquePointer;
+
+    our sub pdf_status_destroy(OpaquePointer) is export(:subs, :DEFAULT) is native(&find-lib) { * }
+    my constant pdf_status_t is export(:types, :DEFAULT) = OpaquePointer but PdfAlloced[&pdf_status_destroy];
+
+    our sub pdf_stm_destroy(OpaquePointer) is export(:subs, :DEFAULT) is native(&find-lib) { * }
+    my constant pdf_stm_t is export(:types, :DEFAULT) = OpaquePointer but PdfAlloced[&pdf_stm_destroy];
 
     our sub pdf_error_get_status(pdf_error_t is rw)
         returns pdf_status_t
--- t/object.t
+++ t/object.t
@@ -8,6 +8,8 @@ my pdf_hash_t $hash-obj;
 lives-ok {$hash-obj = to-pdf-obj( {} ) }, 'to-pdf-obj({}) - lives';
 isa-ok $hash-obj, pdf_hash_t;
 
+lives-ok {$hash-obj.destroy}, 'hash-obj.destroy - lives';
+
 done-testing;
 
 
